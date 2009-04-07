ed1ddbca09a11e943ae374080ed015a0ee0aa61e
Ari Lerner <arilerner@mac.com>
2009-04-06 21:02:38 -0700

    Removed requiring dslify

--- lib/butterfly.rb
+++ lib/butterfly.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-%w(rubygems thin dslify rack/cache).each do |lib|
+%w(rubygems thin rack/cache).each do |lib|
   require lib
 end
 
